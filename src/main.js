7baabb25ef2e497f1bb223a9cbeca8394013d4f6
tcpgnl <i@tcpgnl.com>
2021-08-27 13:52:11 +0800

    Revert "[tcpgnl] feat: add axios  router vuex"
    This reverts commit b06f324f

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus';
-import router from "./router";
-import 'element-plus/dist/index.css'
-import App from './App.vue';
-// import './index.css'
+import App from './App.vue'
+import './index.css'
 
-const app = createApp(App)
-app.use(ElementPlus)
-app.use(router)
-app.mount('#app')
+createApp(App).mount('#app')
