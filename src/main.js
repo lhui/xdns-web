b06f324f7abbd887e2b86639e66193ef83c2e69d
tcpgnl <i@tcpgnl.com>
2021-08-27 13:17:37 +0800

    [tcpgnl] feat: add axios  router vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import ElementPlus from 'element-plus';
+import router from "./router";
+import 'element-plus/dist/index.css'
+import App from './App.vue';
+// import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
