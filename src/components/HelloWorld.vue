50376e6deaa45ac426c5dc68767a4ae4dd3260b7
tcpgnl <i@tcpgnl.com>
2021-08-28 21:35:00 +0800

    [tcpgnl] feat: modify file name

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,19 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <!-- <h1>{{ msg }}</h1> -->
+  <h1>{{ main_name }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'xdns',
   props: {
     msg: String
   },
   data() {
     return {
+      main_name: "XDNS",
       count: 0
     }
   }
