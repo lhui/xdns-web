795a62c2bd0868e473e9e92aa32847444079508b
tcpgnl <i@tcpgnl.com>
2021-09-03 23:38:40 +0800

    [tcpgnl] feat: change table to demo dns data

--- src/components/DNSList.vue
+++ src/components/DNSList.vue
@@ -68,11 +68,11 @@
 
     <el-main>
       <el-table :data="tableData">
-        <el-table-column prop="date" label="日期" width="140">
+        <el-table-column prop="dns_address" label="网址" width="200">
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="120">
+        <el-table-column prop="dns_type" label="记录类型" width="120">
         </el-table-column>
-        <el-table-column prop="address" label="地址">
+        <el-table-column prop="dns_content" label="内容">
         </el-table-column>
       </el-table>
     </el-main>
@@ -85,9 +85,9 @@
        name: "DNSList",
     data() {
       const item = {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
+        dns_address: 'demo.example.com',
+        dns_type: 'A',
+        dns_content: '1.1.1.1'
       };
       return {
         tableData: Array(20).fill(item)
