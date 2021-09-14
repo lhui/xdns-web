bd2124a3299aee41e18e91e09f546b423c2714dc
tcpgnl <i@tcpgnl.com>
2021-09-14 23:16:15 +0800

    [tcpgnl] chore: change name for link

--- src/components/DNSList.vue
+++ src/components/DNSList.vue
@@ -3,32 +3,21 @@
     :data="tableData"
     style="width: 100%"
     height="800px"
-    max-height="100%">
-    <el-table-column
-      fixed
-      prop="dns_address"
-      label="域名"
-      width="300">
+    max-height="100%"
+  >
+    <el-table-column fixed prop="dns_name" label="域名" width="300">
     </el-table-column>
-    <el-table-column
-      prop="dns_type"
-      label="类型"
-      width="120">
+    <el-table-column prop="dns_type" label="类型" width="120">
     </el-table-column>
-    <el-table-column
-      prop="dns_content"
-      label="内容"
-      width="120">
+    <el-table-column prop="dns_content" label="内容" width="120">
     </el-table-column>
-    <el-table-column
-      fixed="right"
-      label="操作"
-      width="120">
+    <el-table-column fixed="right" label="操作" width="120">
       <template #default="scope">
         <el-button
           @click.prevent="deleteRow(scope.$index, tableData)"
           type="text"
-          size="small">
+          size="small"
+        >
           移除
         </el-button>
       </template>
@@ -37,36 +26,36 @@
 </template>
 
 <script>
-  export default {
-       name: "DNSList",
-      methods: {
-      deleteRow(index, rows) {
-        rows.splice(index, 1);
-      }
+export default {
+  name: "DNSList",
+  methods: {
+    deleteRow(index, rows) {
+      rows.splice(index, 1);
     },
-    data() {
-      const item = {
-        dns_address: 'demo.example.com',
-        dns_type: 'A',
-        dns_content: '1.1.1.1'
-      };
-      return {
-        tableData: Array(20).fill(item)
-      }
-    }
-  };
+  },
+  data() {
+    const item = {
+      dns_name: "demo.example.com",
+      dns_type: "A",
+      dns_content: "1.1.1.1",
+    };
+    return {
+      tableData: Array(20).fill(item),
+    };
+  },
+};
 </script>
 
 <style>
 .el-header {
-    background-color: #409EFF;
-    color: var(--el-text-color-primary);
-    line-height: 60px;
-  }
+  background-color: #409eff;
+  color: var(--el-text-color-primary);
+  line-height: 60px;
+}
 
-  .el-aside {
-    color: var(--el-text-color-primary);
-  }
+.el-aside {
+  color: var(--el-text-color-primary);
+}
 </style>
 <style>
 </style>
